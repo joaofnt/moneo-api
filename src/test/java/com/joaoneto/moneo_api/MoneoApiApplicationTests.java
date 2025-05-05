648d5a4e99a1f9028bb231b65639f3a7fa110ac3
João neto <joao.fnnt@gmail.com>
2025-05-05 15:22:32 -0300

    inicializando projeto spring moneo api

---
+++ src/test/java/com/joaoneto/moneo_api/MoneoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoneto.moneo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
